5db37b03b32ef0cd2c6e284c109be6ab639bd818
alfasd1 <ahsm.mam6@gmail.com>
2023-03-28 06:54:04 +0430

    Initial commit

---
+++ enternet.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string a; 
+	cout << "esm khodet ro baram benevis ta payami baray shoma befrestam..." << endl;
+	cout << "    ";
+	getline(cin, a);
+	cout << a << " man abolfazl ahmadi sal 1402 ro be shoma tabrik migam va omid varam sal por barekat, salamati ba jibi por pol ro pish ro dashte bashy.";
+}
